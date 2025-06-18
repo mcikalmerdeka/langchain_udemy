12f7788f13668e860c4c3fc4566ab50deefabf1e
Muhammad Cikal Merdeka <mcikalmerdeka@gmail.com>
2025-06-19 03:45:42 +0700

    refactor for readable format

--- ice_breaker_app.py
+++ ice_breaker_app.py
@@ -4,32 +4,43 @@ from langchain.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-# Load environment variables
-load_dotenv()
+# Create function to get the Linkedin data
+def ice_break_with(name: str) -> str:
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
 
-# Initialize the LLM
-llm = ChatOpenAI(model="gpt-4.1", temperature=0, api_key=os.getenv("OPENAI_API_KEY"))
+    # Get the Linkedin data from third-party tool
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
-# Create a prompt template
-summary_template = """
-Given the Linkedin information {information} about a person, I want you to create two things:
+    # Create a prompt template
+    summary_template = """
+    Given the Linkedin information {information} about a person, I want you to create two things:
 
-1. A short summary of the person's life
-2. Two interesting facts about the person
-"""
+    1. A short summary of the person's life
+    2. Two interesting facts about the person
+    """
 
-prompt = PromptTemplate(
-    input_variables=["information"], # The input variables for the prompt
-    template=summary_template,       # The template for the prompt
-)
+    prompt = PromptTemplate(
+        input_variables=["information"], # The input variables for the prompt
+        template=summary_template,       # The template for the prompt
+    )
 
-# Create a chain (chain of prompts and LLMs with output parsers)
-chain = prompt | llm | StrOutputParser()
+    # Initialize the LLM
+    llm = ChatOpenAI(model="gpt-4.1", temperature=0, api_key=os.getenv("OPENAI_API_KEY"))
 
-# Get the Linkedin data from third-party tool
-linkedin_data = scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/mcikalmerdeka/")
+    # Create a chain (chain of prompts and LLMs with output parsers)
+    chain = prompt | llm | StrOutputParser()
 
-# Run the chain
-res = chain.invoke(input={"information": linkedin_data})
-print(res)
+    # Run the chain
+    res = chain.invoke(input={"information": linkedin_data})
+    print(res)
+
+if __name__ == "__main__":
+    # Load environment variables
+    load_dotenv()
+
+    print("Ice breaker app is running...")
+
+    # Run the ice breaker app
+    ice_break_with(name="Cikal Merdeka")
