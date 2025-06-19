a86df4401fa3825bd9eda70c5ce539a2617318f3
Muhammad Cikal Merdeka <mcikalmerdeka@gmail.com>
2025-06-19 22:04:00 +0700

    append twitter data to ice breaker app context

--- ice_breaker_app.py
+++ ice_breaker_app.py
@@ -3,27 +3,36 @@ from dotenv import load_dotenv
 from langchain.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
+
+
 from third_parties.linkedin import scrape_linkedin_profile
+from third_parties.twitter import scrape_user_tweets
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from agents.twitter_lookup_Agent import lookup as twitter_lookup_agent
 
 # Create function to get the Linkedin data
 def ice_break_with(name: str) -> str:
-    linkedin_profile_url = linkedin_lookup_agent(name=name)
 
-    # Get the Linkedin data from third-party tool
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
+    # Get the Linkedin information
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+
+    # Get the Twitter information
+    twitter_username = twitter_lookup_agent(name=name)
+    tweets = scrape_user_tweets(username=twitter_username)
 
     # Create a prompt template
     summary_template = """
-    Given the Linkedin information {information} about a person, I want you to create two things:
+    Given the information about a person from Linkedin {linkedin_data} and Twitter posts {tweets}, 
+    I want you to create a short summary of the person's life.
 
     1. A short summary of the person's life
     2. Two interesting facts about the person
     """
 
     prompt = PromptTemplate(
-        input_variables=["information"], # The input variables for the prompt
-        template=summary_template,       # The template for the prompt
+        input_variables=["linkedin_data", "tweets"], # The input variables for the prompt
+        template=summary_template,                                                 # The template for the prompt
     )
 
     # Initialize the LLM
@@ -33,7 +42,8 @@ def ice_break_with(name: str) -> str:
     chain = prompt | llm | StrOutputParser()
 
     # Run the chain
-    res = chain.invoke(input={"information": linkedin_data})
+    res = chain.invoke(input={"linkedin_data": linkedin_data, 
+                              "tweets": tweets})
     print(res)
 
 if __name__ == "__main__":
@@ -43,4 +53,7 @@ if __name__ == "__main__":
     print("Ice breaker app is running...")
 
     # Run the ice breaker app
-    ice_break_with(name="Cikal Merdeka")
+    ice_break_with(name="Eden Marco Udemy")
+    
+    # If using my profile (only works with Linkedin though)
+    # ice_break_with(name="Cikal Merdeka")
