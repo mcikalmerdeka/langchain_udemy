5eef8a784d80c91d7e4d5205602364b61c01f06e
Muhammad Cikal Merdeka <mcikalmerdeka@gmail.com>
2025-06-22 00:46:54 +0700

    change function to return value instead of print for the app usage

--- ice_breaker_app.py
+++ ice_breaker_app.py
@@ -1,25 +1,25 @@
 import os
+from typing import Tuple
 from dotenv import load_dotenv
 from langchain.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser # Only used if we dont use pydantic for output parsing
-
 from third_parties.linkedin import scrape_linkedin_profile
 from third_parties.twitter import scrape_user_tweets
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from agents.twitter_lookup_Agent import lookup as twitter_lookup_agent
-from output_parsers import summary_parser
+from output_parsers import summary_parser, Summary
 
 # Create function to get the Linkedin data
-def ice_break_with(name: str) -> str:
+def ice_break_with(name: str) -> Tuple[Summary, str]:
 
     # Get the Linkedin information
     linkedin_username = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
 
     # Get the Twitter information
     twitter_username = twitter_lookup_agent(name=name)
-    tweets = scrape_user_tweets(username=twitter_username, mock=True)
+    tweets = scrape_user_tweets(username=twitter_username)
 
     # Create a prompt template
     summary_template = """
@@ -40,16 +40,17 @@ def ice_break_with(name: str) -> str:
         partial_variables={"format_instructions": summary_parser.get_format_instructions()} # The format instructions for the prompt
     )
 
-    # Initialize the LLM
+        # Initialize the LLM
     llm = ChatOpenAI(model="gpt-4.1", temperature=0, api_key=os.getenv("OPENAI_API_KEY"))
 
     # Create a chain (chain of prompts and LLMs with output parsers)
     chain = summary_prompt_template | llm | summary_parser
 
     # Run the chain
-    res = chain.invoke(input={"linkedin_data": linkedin_data, 
+    res: Summary = chain.invoke(input={"linkedin_data": linkedin_data, 
                               "tweets": tweets})
-    print(res)
+    
+    return res, linkedin_data.get("photoUrl")
 
 if __name__ == "__main__":
     # Load environment variables
