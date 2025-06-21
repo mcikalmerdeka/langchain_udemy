355ce9314dfc3195f39d2236c108a0565e706871
Muhammad Cikal Merdeka <mcikalmerdeka@gmail.com>
2025-06-22 00:47:13 +0700

    Update linkedin.py

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -11,11 +11,10 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
     """
 
     if mock:
-        linkedin_profile_url = "https://gist.githubusercontent.com/mcikalmerdeka/89c47d38232381cdbcbb552851be6425/raw/6150c766bd5a1b7d32657fd8ebc3a11c0c630404/cikal-merdeka-scrapin.json"
+        # If using the course example, use this url
+        linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/859ec7d786b45d8e3e3f688c6c9139d8/raw/5eaf8e46dc29a98612c8fe0c774123a7a2ac4575/eden-marco-scrapin.json"        
+        # linkedin_profile_url = "https://gist.githubusercontent.com/mcikalmerdeka/89c47d38232381cdbcbb552851be6425/raw/6150c766bd5a1b7d32657fd8ebc3a11c0c630404/cikal-merdeka-scrapin.json"
 
-        # # If using the course example, use this url
-        # linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/859ec7d786b45d8e3e3f688c6c9139d8/raw/5eaf8e46dc29a98612c8fe0c774123a7a2ac4575/eden-marco-scrapin.json"
-        
         response = requests.get(
             url=linkedin_profile_url,
             timeout=30
