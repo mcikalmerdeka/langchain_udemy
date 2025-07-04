c642df10b07d9691610cbc81b9869fefa7558d93
Muhammad Cikal Merdeka <mcikalmerdeka@gmail.com>
2025-07-04 19:20:36 +0700

    Add revise answer chain with new prompt instructions
    Introduces a revise_chain using a new prompt template for answer revision, including instructions for incorporating critique, numerical citations, and a references section. Imports ReviseAnswer from schemas and updates the prompt logic to support answer revision.

--- langgraph_reflexion_agent/chains.py
+++ langgraph_reflexion_agent/chains.py
@@ -11,7 +11,7 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph, START, END
 
-from schemas import Reflection, AnswerQuestion
+from schemas import Reflection, AnswerQuestion, ReviseAnswer
 
 load_dotenv()
 
@@ -39,13 +39,26 @@ Current time: {time}
     ]
 ).partial(time=lambda: datetime.datetime.now().isoformat()) # Populate the time variable with the current time
 
-# First Responder Prompt Template
+# First responser prompt template
 first_responder_prompt_template = actor_prompt_template.partial(first_instruction="Provide a detailed ~250 word answer")
 
-# First Responder chain
+# First responder chain
 first_responder_chain = first_responder_prompt_template | llm.bind_tools(
     [AnswerQuestion], tool_choice="AnswerQuestion") # The tool_choice will force the model to use the AnswerQuestion tool
 
+# Add revision instruction to the prompt template (this will be plugged into the {first_instruction} variable)
+revise_instructions = """Revise your previous answer using the new information.
+    - You should use the previous critique to add important information to your answer.
+        - You MUST include numerical citations in your revised answer to ensure it can be verified.
+        - Add a "References" section to the bottom of your answer (which does not count towards the word limit). In form of:
+            - [1] https://example.com
+            - [2] https://example.com
+    - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
+"""
+
+# Add revision chain
+revise_chain = actor_prompt_template.partial(first_instruction=revise_instructions) | llm.bind_tools(
+    tools=[ReviseAnswer], tool_choice="ReviseAnswer")
 
 if __name__ == "__main__":
     print("Hello Langgraph Reflexion Agent")
