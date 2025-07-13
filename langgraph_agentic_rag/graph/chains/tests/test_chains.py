f95d83a4ddc17ccb13b07d445e6600ce065a4f37
Muhammad Cikal Merdeka <mcikalmerdeka@gmail.com>
2025-07-13 22:12:12 +0700

    Add tests for retrieval_grader binary scoring
    Introduces tests for the retrieval_grader to verify correct binary scoring for relevant and irrelevant questions.

--- langgraph_agentic_rag/graph/chains/tests/test_chains.py
+++ langgraph_agentic_rag/graph/chains/tests/test_chains.py
@@ -1,2 +1,30 @@
-def test_foo() -> None:
-    assert 1 == 1
+"""
+Important thing to remember: To run this go to the root directory and run the command:
+pytest -s -v
+"""
+
+from dotenv import load_dotenv
+from langgraph_agentic_rag.graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
+from langgraph_agentic_rag.ingestion import retriever
+
+load_dotenv()
+
+# Define test for yes answer
+def test_retrieval_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_text = docs[0].page_content # Get the most relevant document
+
+    res: GradeDocuments = retrieval_grader.invoke({"question": question, "document": doc_text})
+
+    assert res.binary_score == "yes"
+
+# Define test for no answer
+def test_retrieval_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_text = docs[0].page_content # Get the most relevant document
+
+    res: GradeDocuments = retrieval_grader.invoke({"question": "how to make a pizza", "document": doc_text})
+
+    assert res.binary_score == "no"
