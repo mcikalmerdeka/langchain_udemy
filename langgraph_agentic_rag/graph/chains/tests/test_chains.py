888124bc034a7ce9ee0659ded2ff07eaf10d6f0f
Muhammad Cikal Merdeka <mcikalmerdeka@gmail.com>
2025-07-15 02:03:49 +0700

    Add tests for hallucination grader
    Introduces tests for the hallucination grader to verify both grounded and ungrounded answer scenarios. Ensures the grader correctly identifies when a generation is supported by the provided documents.

--- langgraph_agentic_rag/graph/chains/tests/test_chains.py
+++ langgraph_agentic_rag/graph/chains/tests/test_chains.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
 from graph.chains.generation import generation_chain
 from ingestion import retriever
+from graph.chains.hallucination_grader import hallucination_grader, HallucinationGrader
+
 load_dotenv()
 import pprint
 
@@ -29,7 +31,6 @@ def test_retrieval_grader_answer_no() -> None:
 
     assert res.binary_score == "no"
 
-
 # Define test for generation chain
 def test_generation_chain() -> None:
     question = "agent memory"
@@ -37,3 +38,26 @@ def test_generation_chain() -> None:
     res: str = generation_chain.invoke({"question": question, "context": docs})
     pprint.pprint(res)
 
+# Define the test for hallucination grader yes (means the answer is grounded in the facts)
+def test_hallucination_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    generation = generation_chain.invoke({"question": question, "context": docs})
+
+    res: HallucinationGrader = hallucination_grader.invoke({"documents": docs, "generation": generation})
+
+    assert res.binary_score
+
+# Define the test for hallucination grader no (means the answer is not grounded in the facts)
+def test_hallucination_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    res: HallucinationGrader = hallucination_grader.invoke(
+        {
+            "documents": docs,
+            "generation": "In order to make pizza we need to first start with the dough",
+        }
+    )
+    assert not res.binary_score
