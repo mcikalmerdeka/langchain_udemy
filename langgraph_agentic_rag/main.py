bcf813d9ae9ba41833d07983d3312a483fd5e061
Muhammad Cikal Merdeka <mcikalmerdeka@gmail.com>
2025-07-15 05:20:03 +0700

    final testing on the app for 4 different input question

--- langgraph_agentic_rag/main.py
+++ langgraph_agentic_rag/main.py
@@ -7,11 +7,11 @@ load_dotenv()
 if __name__ == "__main__":
     print("Hello From Langgraph Agentic RAG Application")
 
-    # # Experiment 1: Agent memory (inside the knowledge store, from the first url source)
-    # print(rag_app.invoke(input={"question": "What is agent memory?"}))
+    # Experiment 1: Agent memory (inside the knowledge store, from the first url source)
+    print(rag_app.invoke(input={"question": "What is agent memory?"}))
 
-    # Experiment 2: Few-show prompting (inside the knowledge store, from the second url source)
-    print(rag_app.invoke(input={"question": "Can you explain the concept of few-shot prompting?"}))
+    # # Experiment 2: Few-show prompting (inside the knowledge store, from the second url source)
+    # print(rag_app.invoke(input={"question": "Can you explain the concept of few-shot prompting?"}))
 
     # # Experiment 3: Definition of Microsoft AI search service (outside the knowledge store, need to run web search node)
     # print(rag_app.invoke(input={"question": "what is the definition of Microsoft AI search service?"}))
