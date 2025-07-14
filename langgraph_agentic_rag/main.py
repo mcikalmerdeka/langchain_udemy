de9f0a75e80386e590ac0becbc32727e4f21e106
Muhammad Cikal Merdeka <mcikalmerdeka@gmail.com>
2025-07-14 22:37:10 +0700

    finalize the main script for the app to run
    Replaces previous imports with rag_app from graph.graph and updates the main entry point to run example experiments using rag_app.invoke. Adds commented-out experiments for agent memory and Microsoft AI search service, and runs an experiment for places to visit in Indonesia.

--- langgraph_agentic_rag/main.py
+++ langgraph_agentic_rag/main.py
@@ -1,12 +1,17 @@
 import os
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain_chroma import Chroma
+from graph.graph import rag_app
 
 load_dotenv()
 
-
-
-
 if __name__ == "__main__":
-    print("Hello From Langgraph Agentic RAG")
+    print("Hello From Langgraph Agentic RAG Application")
+
+    # # Experiment 1: Agent memory (inside the knowledge store)
+    # print(rag_app.invoke(input={"question": "What is agent memory?"}))
+
+    # # Experiment 2: Definition of Microsoft AI search service (outside the knowledge store, need to run web search node)
+    # print(rag_app.invoke(input={"question": "what is the definition of Microsoft AI search service?"}))
+
+    # Experiment 3: Places to visit in Indonesia (outside the knowledge store and out of topic, need to run web search node)
+    print(rag_app.invoke(input={"question": "what are the places to visit in Indonesia?"}))
