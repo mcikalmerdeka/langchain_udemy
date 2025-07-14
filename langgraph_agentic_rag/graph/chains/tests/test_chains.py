973ded1a9548ae902bff9cc36ebbe6f0bb3aa6a1
Muhammad Cikal Merdeka <mcikalmerdeka@gmail.com>
2025-07-14 20:49:44 +0700

    add testing for the generation chain

--- langgraph_agentic_rag/graph/chains/tests/test_chains.py
+++ langgraph_agentic_rag/graph/chains/tests/test_chains.py
@@ -4,8 +4,10 @@ pytest -s -v
 """
 from dotenv import load_dotenv
 from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
+from graph.chains.generation import generation_chain
 from ingestion import retriever
 load_dotenv()
+import pprint
 
 # Define test for yes answer
 def test_retrieval_grader_answer_yes() -> None:
@@ -26,3 +28,12 @@ def test_retrieval_grader_answer_no() -> None:
     res: GradeDocuments = retrieval_grader.invoke({"question": "how to make a pizza", "document": doc_text})
 
     assert res.binary_score == "no"
+
+
+# Define test for generation chain
+def test_generation_chain() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    res: str = generation_chain.invoke({"question": question, "context": docs})
+    pprint.pprint(res)
+
